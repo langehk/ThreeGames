002f64236b9c967238f4a206f550b07b7b0b5f6d
Morten Højrup Kristensen <morkr@mentor-it.dk>
2020-09-23 11:12:50 +0200

    Update Yatzy.js

--- script/Yatzy.js
+++ script/Yatzy.js
@@ -173,7 +173,7 @@ function rollDices(arr) {
     let calcSixesResult = calcSixes(arr);
     let calcOnePairResult = onePair(arr);
     let calcTwoPairResult = twoPair(arr);
-    debugger;
+    
      
      
     return arr;
